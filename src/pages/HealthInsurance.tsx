08bbfd8efb3c6db4453627b1cbbbcaf923414bcb
Priv2Konrad <kkplak@gmail.com>
2025-01-27 14:14:44 +0100

    work in progress on formatting the json

--- src/pages/HealthInsurance.tsx
+++ src/pages/HealthInsurance.tsx
@@ -3,36 +3,63 @@ import { useTranslation } from "react-i18next";
 
 const HealthInsurance: React.FC = () => {
   const { t } = useTranslation();
+  
   return (
     <>
     <div className='insurance-page container'>
-      <h1>{t("healthInsurance")}</h1>
-      <p>{t("healthInfoP")}</p>
+      <h1>{t("HEALTH.title")}</h1>
+      <p>{t("HEALTH.info")}</p>
+      <p>{t("HEALTH.infoList")}</p>
+      <ol>
+        <li>{t("HEALTH.infoListItem1")}</li>
+        <li>{t("HEALTH.infoListItem2")}</li>
+        </ol>
 
 
    
       <div className='insurance-page container2'>
-      <p>{t("healthInfoUP")}</p>
+      <p>{t("HEALTH.basic.title")}</p>
+      <p>{t("HEALTH.basic.p")}</p>
+      <p>{t("HEALTH.basic.p1")}</p>
+      <p>{t("HEALTH.basic.l1")}</p>
+      <p>{t("HEALTH.basic.l2")}</p>
+      <p>{t("HEALTH.basic.l3")}</p>
+      <p>{t("HEALTH.basic.l4")}</p>
+      <p>{t("HEALTH.basic.l5")}</p>
+      <p>{t("HEALTH.basic.p2")}</p>
+      <p>{t("HEALTH.basic.pl1")}</p>
+      <p>{t("HEALTH.basic.pl2")}</p>
+      
    
-      <p>{t("healthInfoFran")}</p>
+      <p>{t("HEALTH.fran.title")}</p>
+      <p>{t("HEALTH.fran.p")}</p>
+      <p>{t("HEALTH.fran.p1")}</p>
+      <p>{t("HEALTH.fran.p2")}</p>
+      <p>{t("HEALTH.fran.p3")}</p>
+      <p>{t("HEALTH.fran.p4")}</p>
+      <p>{t("HEALTH.fran.p5")}</p>
+      <p>{t("HEALTH.fran.p6")}</p>
+      <p>{t("HEALTH.fran.p7")}</p>
+      <p>{t("HEALTH.fran.p8")}</p>
+   
     
-      <p>{t("healthInfoModel")}</p>
+      <p>{t("HEALTH.healthInfoModel")}</p>
   
-      <p>{t("healthInfoD")}</p>
+      <p>{t("HEALTH.healthInfoD")}</p>
 
-      <p>{t("healthInfoW")}</p>
+      <p>{t("HEALTH.healthInfoW")}</p>
       </div>
 
       <div className='insurance-page container2'>
-      <h2>{t("healthNewH2")}</h2>
-      <p>{t("healthNewP")}</p>
-      <p>{t("healthNewP2")}</p>
-      <p><b>{t("healthNewPItem1")}</b></p>
-      <p className="smaller-p">{t("healthNewP3")}</p>
-      <p><b>{t("healthNewPItem2")}</b></p>
-      <p><b>{t("healthNewPItem3")}</b></p>
-      <p><b>{t("healthNewPItem4")}</b></p>
-      <p>{t("healthNewP4")}</p>
+      <h2>{t("HEALTH.newcommers.h2")}</h2>
+      <p>{t("HEALTH.newcommers.p")}</p>
+      <p>{t("HEALTH.newcommers.p2")}</p>
+      <p><b>{t("HEALTH.newcommers.item1")}</b></p>
+      <p className="smaller-p">{t("HEALTH.newcommers.p3")}</p>
+      <p><b>{t("HEALTH.newcommers.item2")}</b></p>
+      <p><b>{t("HEALTH.newcommers.item3")}</b></p>
+      <p><b>{t("HEALTH.newcommers.item4")}</b></p>
+      <p>{t("HEALTH.newcommers.p4")}</p>
       </div>
           </div>
     </>
